fdc4cf3cbfa61e2bf3cb3f092a6743f247b14d1c
Shubham Parihar <shubhamparihar391@gmail.com>
2021-09-14 21:10:11 +0530

    chore: add og meta tags and navbar links

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,8 +3,35 @@ import { description } from '../../package.json';
 import type { DefaultThemeOptions, ViteBundlerOptions} from 'vuepress-vite';
 
 export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
-    lang: 'en-UK',
-    title: 'Twitter.js Guide',
-    description: description,
-    bundler: '@vuepress/vite',
+  lang: 'en-UK',
+  title: 'Twitter.js Guide',
+  description: description,
+  bundler: '@vuepress/vite',
+  head: [
+    ['meta', { property: 'og:title', content: 'Twitter.js Guide' }],
+    ['meta', { property: 'og:description', content: description }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:image', content: '/branding/logo.png' }],
+    ['meta', { property: 'og:locale', content: 'en_UK' }],
+    ['meta', { name: 'theme-color', content: '#f7df1e' }],
+  ],
+  themeConfig: {
+    repo: 'https://github.com/twitterjs/twitter.js',
+    docsRepo: 'https://github.com/twitterjs/guide',
+    docsDir: 'src',
+    navbar: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'API',
+        link: 'https://twitter.js.org'
+      },
+      {
+        text: 'Discord',
+        link: 'https://discord.gg/f5Pefuskx4'
+      }
+    ]
+  }
 })
