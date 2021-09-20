71e024ba17820d972ffcccff38152e850ef9bbac
Shubham Parihar <shubhamparihar391@gmail.com>
2021-09-20 22:42:17 +0530

    chore: use full url for twitter:image meta tag

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -16,7 +16,7 @@ export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
     ['meta', { name: 'theme-color', content: '#f7df1e' }],
     ['meta', { name: 'twitter:title', content: 'Twitter.js Guide'}],
     ['meta', { name: 'twitter:description', content: 'The official guide for twitter.js library'}],
-    ['meta', { name: 'twitter:image', content: '/branding/logo.png'}],
+    ['meta', { name: 'twitter:image', content: 'https://twitterjs-guide.pages.dev/branding/logo.png'}],
     ['meta', { name: 'twitter:card', content: 'summary'}],
   ],
   themeConfig: {
